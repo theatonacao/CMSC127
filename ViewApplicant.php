5989007ac5fd529f8c6fa611e516eea2684d527b
theatonacao <60372273+theatonacao@users.noreply.github.com>
2020-02-14 19:34:13 +0800

    Update ViewApplicant.php

--- ViewApplicant.php
+++ ViewApplicant.php
@@ -1,4 +1,4 @@
-<html>
+  <html>
     <head>
         <title>Kasama</title>
         <link rel="stylesheet" type="text/css" href="style.css">
@@ -24,33 +24,35 @@ if ($conn->connect_error) {
 } 
 
 // sql to delete a record
-$sql = "SELECT surname, firstname, middlename, nameextension, bday, placeofbirth, sex, civilstatus, height, weight, bloodtype, gsis, pagibig, philhealth, sss, tin, agencyemployee, citizenship, res_add, per_add, telephone, mobile, email FROM datas WHERE gsis = '$id'";
-$result = $conn->query($sql);
+//$sql = "SELECT surname, firstname, middlename, nameextension, bday, placeofbirth, sex, civilstatus, height, weight, bloodtype, gsis, pagibig, philhealth, sss, tin, agencyemployee, citizenship, res_add, per_add, telephone, mobile, email FROM datas WHERE gsis = '$id'";
+//$result = $conn->query($sql);
+
+$result = mysqli_query($conn, "SELECT  * FROM datas WHERE gsis = '$id'");
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
 		echo '<br>';
-        echo "<h4 align=left >Name: " . $row["firstname"] . " " . $row["middlename"] . " " . $row["surname"] . "<br>";
-        echo "Date of birth: " . $row["bday"]. "<br>";
-        echo "Place of birth: " . $row["placeofbirth"]. "<br>";
-        echo "Sex: " . $row["sex"]. "<br>";
-        echo "Civil Status: " . $row["civilstatus"]. "<br>";
-        echo "Height: " . $row["height"]. "<br>";
-        echo "Weight: " . $row["weight"]. "<br>";
-        echo "Blood Type: " . $row["bloodtype"]. "<br>";
-        echo "GSIS ID Number: " . $row["gsis"]. "<br>";
-        echo "Pagibig ID Number: " . $row["pagibig"]. "<br>";
-        echo "Philhealth Number: " . $row["philhealth"]. "<br>";
-        echo "SSS Number: " . $row["sss"]. "<br>";
-        echo "TIN Number: " . $row["tin"]. "<br>";
-        echo "Agency Employee Number: " . $row["agencyemployee"]. "<br>";
-        echo "Citizenship: " . $row["citizenship"]. "<br>";
-        echo "Residential Address: " . $row["res_add"]. "<br>";
-        echo "Permanent Address: " . $row["per_add"]. "<br>";
-        echo "Telephone Number: " . $row["telephone"]. "<br>";
-        echo "Mobile Number: " . $row["mobile"]. "<br>";
-        echo "Email Address: " . $row["email"]. "<br></h4>";
+        echo "<h4 align=left >Name: " . $row["Firstname"] . " " . $row["Middlename"] . " " . $row["Surname"] . "<br>";
+        echo "Date of birth: " . $row["Bday"]. "<br>";
+        echo "Place of birth: " . $row["Placeofbirth"]. "<br>";
+        echo "Sex: " . $row["Sex"]. "<br>";
+        echo "Civil Status: " . $row["Civil_Status"]. "<br>";
+        echo "Height: " . $row["Height"]. "<br>";
+        echo "Weight: " . $row["Weight"]. "<br>";
+        echo "Blood Type: " . $row["Bloodtype"]. "<br>";
+        echo "GSIS ID Number: " . $row["GSIS"]. "<br>";
+        echo "Pagibig ID Number: " . $row["PAGIBIG"]. "<br>";
+        echo "Philhealth Number: " . $row["PhilHealth"]. "<br>";
+        echo "SSS Number: " . $row["SSS"]. "<br>";
+        echo "TIN Number: " . $row["TIN"]. "<br>";
+        echo "Agency Employee Number: " . $row["EmployeeNo"]. "<br>";
+        echo "Citizenship: " . $row["Citizenship"]. "<br>";
+        echo "Residential Address: " . $row["Residence_add"]. "<br>";
+        echo "Permanent Address: " . $row["Permanent_add"]. "<br>";
+        echo "Telephone Number: " . $row["Telephone"]. "<br>";
+        echo "Mobile Number: " . $row["Mobile"]. "<br>";
+        echo "Email Address: " . $row["Email"]. "<br></h4>";
     }
 } else {
     echo "<h4>0 results</h4>";
